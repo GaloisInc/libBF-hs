ccd3f138338b9cc36bfc850213657236963831d7
Matthew Peddie <mpeddie@gmail.com>
2021-06-01 12:03:09 +1000

    Bring out libbf support for transcendentals
    `libbf/readme.txt` calls out the following limitations of the
    transcendental functions:
    
        > - In some operations (such as the transcendental ones), there is
        >   no rigourous proof of the rounding error. We expect to improve
        >   it by reusing ideas from the MPFR algorithms. Some unlikely
        >   overflow/underflow cases are also not handled in exp or pow.
        >
        > - The transcendental operations are not speed optimized and do not
        >   use an asymptotically optimal algorithm (the running time is in
        >   O(n^(1/2)*M(n)) where M(n) is the time to multiply two n bit
        >   numbers). A possible solution would be to implement a binary
        >   splitting algorithm for exp and sin/cos (see [1]) and to use a
        >   Newton based inversion to get log and atan.
        >
        > - Memory allocation errors are not always correctly reported for
        >   the transcendental operations.
    
    However, they're in the library, and it would be useful to have access
    to these basic functions in SBV.

--- tests/RunUnitTests.hs
+++ tests/RunUnitTests.hs
@@ -1,4 +1,5 @@
 {-# Language BlockArguments #-}
+{-# Language LambdaCase #-}
 module Main(main) where
 
 import System.Exit(exitFailure)
@@ -15,6 +16,10 @@ main =
      print $ bfFromString 10 (expBits 3 <> precBits 2 <> rnd ToZero) "1.0e200"
      dblTest "+" (+) (bfAdd (float64 NearEven)) 1 2
      dblTest "/" (/) (bfDiv (float64 NearEven)) 1 0
+     dblTest "atan2" atan2 (bfAtan2 (float64 NearEven)) 1 2
+     dblTest "atan2" atan2 (bfAtan2 (float64 NearEven)) 2 1
+     checkPredicate "sin" (bfSin (float256 NearEven)) (bfIsZero) (bfFromDouble 0)
+     checkPredicate "exp" (bfExp (float256 NearEven)) (== (bfFromDouble 1)) (bfFromDouble 0)
 
 check :: String -> Bool -> IO ()
 check x b = unless b
@@ -40,5 +45,20 @@ dblTest op opD opBF x y =
             (res1,Ok) -> Right res1
             (_, s)    -> Left ("result: " ++ show s)
 
-
-
+checkPredicate ::
+  Show a =>
+  String ->
+  (a -> (BigFloat, Status)) ->
+  (BigFloat -> Bool) ->
+  a ->
+  IO ()
+checkPredicate opName opBF predicate input = do
+  check
+    ("Status '" ++ show bfStatus ++ "' not OK for " ++ describeOp)
+    (bfStatus == Ok)
+  check
+    ("Test predicate failed on result " ++ show bfRes ++ " on " ++ describeOp)
+    (predicate bfRes)
+  where
+    (bfRes, bfStatus) = opBF input
+    describeOp = opName ++ "(" ++ show input ++ ")"
