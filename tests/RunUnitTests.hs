1071631f1360838707ed2014f967f6cfde0bcf40
Ryan Scott <rscott@galois.com>
2023-07-17 13:27:42 -0400

    Add unit tests for #22
    These currently fail on AArch64, but I will address this in a subsequent
    commit.

--- tests/RunUnitTests.hs
+++ tests/RunUnitTests.hs
@@ -1,6 +1,7 @@
 {-# Language BlockArguments #-}
 module Main(main) where
 
+import Data.Foldable(traverse_)
 import System.Exit(exitFailure)
 import System.IO(hPutStrLn,stderr)
 import Control.Monad(unless)
@@ -15,6 +16,8 @@ main =
      print $ bfFromString 10 (expBits 3 <> precBits 2 <> rnd ToZero) "1.0e200"
      dblTest "+" (+) (bfAdd (float64 NearEven)) 1 2
      dblTest "/" (/) (bfDiv (float64 NearEven)) 1 0
+     traverse_ (\bf -> bfSubnormalTest bf False)
+       [bfPosZero, bfFromInt 1, bfFromInt 0, bfNaN, bfNegInf, bfPosInf]
 
 check :: String -> Bool -> IO ()
 check x b = unless b
@@ -40,5 +43,9 @@ dblTest op opD opBF x y =
             (res1,Ok) -> Right res1
             (_, s)    -> Left ("result: " ++ show s)
 
-
-
+-- Check that calling bfIsSubnormal on a BigFloat value returns the expected
+-- result.
+bfSubnormalTest :: BigFloat -> Bool -> IO ()
+bfSubnormalTest bf expected =
+  check ("bfIsSubnormal (float32 NearEven) " ++ show bf)
+        (bfIsSubnormal (float32 NearEven) bf == expected)
